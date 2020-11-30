2806483539cf560d2d0c175371c888923e0424a9
TSD <tony1@ximalaya.com>
2020-11-30 10:18:39 +0800

    基础语法

---
+++ hello-world/src/components/ClassStyle.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="wrap" :class="{ active: isActive }">
+    <button @click="handleClick">切换颜色</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      isActive: true,
+    };
+  },
+  methods: {
+    handleClick: function () {
+      this.isActive = !this.isActive
+    },
+  },
+};
+</script>
+  
+<style>
+.wrap {
+  width: 300px;
+  height: 300px;
+  background-color: pink;
+}
+.active {
+  background-color: orange;
+}
+</style>
---
+++ hello-world/src/components/reserveMessage.vue
@@ -0,0 +1,24 @@
+<template>
+  <p>{{ reserveMessage }}</p>
+</template>
+
+<script>
+export default {
+  props: {
+    msg: String,
+  },
+  computed: {
+    reserveMessage: {
+      get: function () {
+        
+      },
+      set: function () {
+
+      },
+    },
+  },
+};
+</script>
+
+<style>
+</style>
